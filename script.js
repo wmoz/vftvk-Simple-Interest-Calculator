12354af1768655450b7bcc0dfbc73e84a5484569
wmoz <wmoz@vt.edu>
2022-01-14 16:05:38 +0200

    finished project

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var intr = p *years* (rate/100);
     
+    
+    var year = new Date().getFullYear()+ parseInt(years);
+
+    document.getElementById("result").innerText = "If you deposit "+ p+ ", at an interest rate of " + rate + ". You will receive an amount of " + intr + ", in the year " + year;
+
+
+    
+  
+    
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
         
